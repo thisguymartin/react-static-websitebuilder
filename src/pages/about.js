139800f9128926980e9fbf2daa23af2f38fbdc14
Martin Patino <mpatino117@gmail.com>
2019-11-10 19:30:49 -0700

    updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,13 +6,12 @@ import { Link } from 'components/Router'
 export default function Blog() {
   const { posts } = useRouteData()
   return (
-    <div>
+    <div className="container">
       <h1> About</h1>
 
-      <p>
+      <iframe src="https://giphy.com/embed/aKE8cuxykToFG" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/rickandmorty-adult-swim-rick-and-morty-02x04-aKE8cuxykToFG"></a></p>      <p>
         This is a simple demo for grapejs working inside a react app. Note: Grapejs is not using Jquery compare to other site builder this is a pure OOP Javascript library.
         So for those use to react libaries of funtional programming. Well that part is missing so sorry.
-
       </p>
      
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,9 @@ export default () => (
     <p>Inside a Static React App</p>
 
     <div>
-
-
       <h5>Comming Soon .... Google bucket deployment </h5>
-
-      Repo: <a href="https://github.com/mpatino117/react-static-websitebuilder"></a>
-      <a href="https://5dc8c0f021788d6b0ccbb30b--dreamy-darwin-5e1de6.netlify.com/sitebuilder"> Demo Site </a>
+      Repo: <a href="https://github.com/mpatino117/react-static-websitebuilder">Gihub Repo</a> | 
+      <a href="https://dreamy-darwin-5e1de6.netlify.com/"> Demo Site </a>
     </div>
 
   </div>
