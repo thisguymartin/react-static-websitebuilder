7e511147b2401605275ef1849cd0e9984c75060b
Martin Patino <mpatino117@gmail.com>
2019-11-10 19:35:34 -0700

    spelling

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,8 +10,9 @@ export default function Blog() {
       <h1> About</h1>
 
       <iframe src="https://giphy.com/embed/aKE8cuxykToFG" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/rickandmorty-adult-swim-rick-and-morty-02x04-aKE8cuxykToFG"></a></p>      <p>
-        This is a simple demo for grapejs working inside a react app. Note: Grapejs is not using Jquery compare to other site builder this is a pure OOP Javascript library.
-        So for those use to react libaries of funtional programming. Well that part is missing so sorry.
+      This is a simple demo for Grapejs working inside a react app. Note: Grapejs is not using JQuery compare to other site builder this is a pure OOP Javascript library.
+        So for those use to react libraries of functional programming. Well that part is missing so sorry.
+
       </p>
      
     </div>
